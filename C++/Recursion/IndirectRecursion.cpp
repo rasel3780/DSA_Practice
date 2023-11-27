54fdc753c1e5065c7b1da21f0522f8d7eb614b5c
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-27 12:01:01 +0600

    Indirect Recursion example added

---
+++ C++/Recursion/IndirectRecursion.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void funA(int n);
+void funB(int n);
+
+void funA(int n){
+    if(n>0){
+        cout<<n<<endl;
+        funB(n-1);
+    }
+}
+
+void funB(int n){
+    if(n>1){
+        cout<<n<<endl;
+        funA(n/2);
+    }
+}
+
+int main(){
+    int a=20;
+    funA(a);
+}
