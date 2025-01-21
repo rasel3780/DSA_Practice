2107aede93a5bc2a138a07b4a02c4c66c91449b6
Rasel Kibria <rasel5jkibria@gmail.com>
2025-01-21 22:43:19 +0600

    Linear Search in c# updated

--- C#/LinearSearch/Program.cs
+++ C#/LinearSearch/Program.cs
@@ -1,22 +1,16 @@
-﻿public class LinearSearching
+﻿int linearSearch(int[] A, int n, int target)
 {
-    public static int linearSearch(int[] arr, int arrLen, int target)
+    for(int i = 0; i<n; i++)
     {
-        for(int i = 0; i<arrLen; i++)
+        if(A[i] == target)
         {
-            if (arr[i] == target)
-                return i;
+            return i;
         }
-        return -1;
-    }
-
-    public static void Main(string[] args)
-    {
-        int[] arr = { 1, 5, 4, 6, 3, 8, 10, 9, 2, 7};
-        int res = linearSearch(arr, arr.Length, 8);
-        if(res == -1)
-            Console.WriteLine("Not found");
-        else
-            Console.WriteLine($"Found at index: {res}");
-    }
+    }    
+    
+    return -1;
 }
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+int result = linearSearch(arr, arr.Length, 5);
+Console.WriteLine("Result: "+ result);
